d98ce2c255471cffa93a130f87344960720e9e44
Hyunmin Park <npc@Hyunminui-MacBook-Pro.local>
2017-04-01 09:45:42 +0900

    Write Stack (on going)

---
+++ src/data_structure/Stack_170401.java
@@ -0,0 +1,16 @@
+package data_structure;
+
+public class Stack_170401 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
+
+interface Stack {
+    boolean isEmpty();
+    void push(char item);
+    char pop();
+    void delete();
+    char peek();
+}
